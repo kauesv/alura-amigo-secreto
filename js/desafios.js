ebc31594ba90bc2ed30a0d6d88dd7b0f7600601e
kaue_sv <kauesousavieira534@gmail.com>
2024-07-19 12:53:48 -0300

    Adicionado os desafios

---
+++ js/desafios.js
@@ -0,0 +1,21 @@
+//Crie uma função que valide se um número é positivo, negativo ou zero.
+
+//Implemente uma função que verifique se uma pessoa é maior de idade.
+
+//Desenvolva uma função que valide se uma string é vazia ou não.
+
+//Crie uma função que determine se um ano é bissexto
+
+//Implemente uma função que calcule a média de dois números, interrompendo a execução se algum dos números não for válido.
+
+//Desenvolva uma função que receba um array como parâmetro e retorne o seu tamanho.
+
+//Crie um array e utilize a função includes para verificar se um elemento específico está presente
+
+//Implemente uma função que receba um array e um elemento, e retorne se o elemento está presente no array.
+
+//Crie um array de strings e utilize includes para verificar se uma determinada string está presente.
+
+//Desenvolva uma função que receba um array de objetos representando estudantes de um curso e um objeto que representa um estudante procurado. Depois retorne se o objeto está presente no array.
+
+//Crie uma função que receba um array de números e retorne a soma dos elementos pares e o produto dos elementos ímpares.
